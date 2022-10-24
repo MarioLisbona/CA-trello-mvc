af112db575afa4aef0fee4dbdfa3f0c3788d33d8
Mario Lisbona <mario.lisbona@gmail.com>
2022-10-24 21:48:17 +1100

    need to drop create and seed from main

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from datetime import date
+import os
 from flask import Flask
+from controllers.db_commands import db_commands
 from db import db, ma, bcrypt, jwt
 from controllers.cards_controller import cards_bp
 from controllers.users_controller import users_bp
-import os
-
+from models.card import Card, CardSchema
+from models.user import User, UserSchema
 
 
 def create_app():
@@ -20,5 +23,84 @@ def create_app():
 
     app.register_blueprint(cards_bp)
     app.register_blueprint(users_bp)
+    # app.register_blueprint(db_commands)
 
+    @app.cli.command('drop')
+    def drop_db():
+        db.drop_all()
+        print("Tables dropped")
+
+    @app.cli.command('create')
+    def create_db():
+        db.create_all()
+        print("Tables created")
+
+    @app.cli.command('seed')
+    def seed_db():
+        users = [
+            User(
+                name='Coda Lisbona',
+                email='admin@spam.com',
+                password=bcrypt.generate_password_hash('eggs').decode('utf-8'),
+                is_admin=True
+            ),
+            User(
+                name='John Cleese',
+                email='someone@spam.com',
+                password=bcrypt.generate_password_hash('12345').decode('utf-8')
+            ),
+            User(
+                name='Mario Lisbona',
+                email='mario.lisbona@gmail.com',
+                password=bcrypt.generate_password_hash('Muzza1234').decode('utf-8'),
+                is_admin=True
+            ),
+            User(
+                name='Ali Taubner',
+                email='amtaubner@gmail.com',
+                password=bcrypt.generate_password_hash('aliT123').decode('utf-8'),
+                is_admin=True
+            ),
+            User(
+                name='Coda Cat',
+                email='coda@cat.com',
+                password=bcrypt.generate_password_hash('CodaCatttt!').decode('utf-8')
+            )
+        ]
+
+        cards = [
+            Card(
+                title = 'Start the project',
+                description = 'Stage 1 - Create the database',
+                status = 'To Do',
+                priority = 'High',
+                date = date.today()
+            ),
+            Card(
+                title = "SQLAlchemy",
+                description = "Stage 2 - Integrate ORM",
+                status = "Ongoing",
+                priority = "High",
+                date = date.today()
+            ),
+            Card(
+                title = "ORM Queries",
+                description = "Stage 3 - Implement several queries",
+                status = "Ongoing",
+                priority = "Medium",
+                date = date.today()
+            ),
+            Card(
+                title = "Marshmallow",
+                description = "Stage 4 - Implement Marshmallow to jsonify models",
+                status = "Ongoing",
+                priority = "Medium",
+                date = date.today()
+            )
+        ]
+
+        db.session.add_all(cards)
+        db.session.add_all(users)
+        db.session.commit()
+        print('Tables seeded')
     return app
